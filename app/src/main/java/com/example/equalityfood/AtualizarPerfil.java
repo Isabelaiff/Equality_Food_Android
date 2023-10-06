2216487bfd5e5f7285a70239f2bfb95994e2c977
Isabela Freitas Franco <isabelafranco-ieg@instituto.ad>
2023-10-06 15:37:05 -0300

    Ajustando tela de cadastro

---
+++ app/src/main/java/com/example/equalityfood/AtualizarPerfil.java
@@ -0,0 +1,14 @@
+package com.example.equalityfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AtualizarPerfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atualizar_perfil);
+    }
+}
