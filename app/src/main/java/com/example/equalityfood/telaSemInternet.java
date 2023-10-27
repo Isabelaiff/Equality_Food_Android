6a401ca4a732dc2270c67d23cda3e3632ef8b303
Guilherme Quental Marques <guilhermemarques-ieg@instituto.ad>
2023-10-27 15:16:16 -0300

    subindo tela de sem internet

---
+++ app/src/main/java/com/example/equalityfood/telaSemInternet.java
@@ -0,0 +1,14 @@
+package com.example.equalityfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class telaSemInternet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_sem_internet);
+    }
+}
